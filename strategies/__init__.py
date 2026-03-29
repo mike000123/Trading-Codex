03b9bfe39b38b7918d8a29590454f567ef5591dd
Dimitris Zouzias <57437543+mike000123@users.noreply.github.com>
2026-03-30 01:15:58 +0300

    Add files via upload

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -5,8 +5,9 @@ To add a new strategy: create the file, add the import below.
 from .base import BaseStrategy, register_strategy, get_strategy, list_strategies
 
 # Auto-register all built-in strategies
-from . import rsi_strategy       # noqa: F401
-from . import ma_crossover       # noqa: F401
-from . import macd_strategy      # noqa: F401
+from . import rsi_strategy           # noqa: F401
+from . import ma_crossover           # noqa: F401
+from . import macd_strategy          # noqa: F401
+from . import fixed_level_strategy   # noqa: F401
 
 __all__ = ["BaseStrategy", "register_strategy", "get_strategy", "list_strategies"]
